c80849cb07b57a3f14b1bee4a12f4869eeda3461
Jeshua Ben <jeshuacostafer@gmail.com>
2022-07-04 01:07:44 -0300

    feat: populated the classes that make up the viewHolder

--- app/src/main/java/br/com/jeshuaben/pokedex/view/PokemonAdapter.kt
+++ app/src/main/java/br/com/jeshuaben/pokedex/view/PokemonAdapter.kt
@@ -1,26 +1,39 @@
 package br.com.jeshuaben.pokedex.view
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import br.com.jeshuaben.pokedex.R
 import br.com.jeshuaben.pokedex.domain.Pokemon
 
 class PokemonAdapter(
-    val items: List<Pokemon>
+    private val items: List<Pokemon>
 ) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.pokemon_item, parent, false)
 
+            return ViewHolder(view)
         }
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = items[position]
 
+            holder.bindView(item)
         }
 
         override fun getItemCount() = items.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        fun binView() {
-
+        fun bindView(item: Pokemon) = {with(itemView) {
+            val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+            val tvNumber = findViewById<TextView>(R.id.tvNumber)
+            val tvName = findViewById<TextView>(R.id.tvName)
+            val tvType1 = findViewById<TextView>(R.id.tvType1)
+            val tvType2 = findViewById<TextView>(R.id.tvType2)
+            }
         }
     }
 
